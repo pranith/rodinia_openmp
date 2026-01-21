0699ae45866639111bb23db038122bc70306569c
Pranith Kumar <bobby.prani@gmail.com>
2026-01-21 11:13:32 -0800

    Add data files

---
+++ data/gaussian/matrixGenerator.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:6b1c03771daa7e9841b7a6a23ff50403a8e485755c25b7b516fa795075866d83
+size 2412
---
+++ data/mummergpu/genreads.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ac6d42201a63f07e1dcb4a6b2aa58ad6e59efc2a9ba6897c2841182ea65fa041
+size 1794
